7ee128dfd3ffd54c6953956673e1ff91ddba17c7
UseMuse <UseMuse@mail.ru>
2021-09-15 10:49:33 +0300

    реализация примера https://professorweb.ru/my/WPF/documents_WPF/level36/36_5.php

---
+++ Projects/Projects/Models/Contracts/IProjectsModel.cs
@@ -0,0 +1,19 @@
+﻿using Projects.Args;
+using Projects.Classes;
+using Projects.Classes.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects.Models.Contracts
+{
+    public interface IProjectsModel
+    {
+        ObservableCollection<Project> Projects { get; set; }
+        event EventHandler<ProjectEventArgs> ProjectUpdated;
+        void UpdateProject(IProject updatedProject);
+    }
+}
---
+++ Projects/Projects/Models/ProjectsModel.cs
@@ -0,0 +1,42 @@
+﻿using Projects.Args;
+using Projects.Classes;
+using Projects.Classes.Contracts;
+using Projects.Models.Contracts;
+using Projects.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects.Models
+{
+    public class ProjectsModel : IProjectsModel
+    {
+        public ObservableCollection<Project> Projects { get; set; }
+        public event EventHandler<ProjectEventArgs> ProjectUpdated = delegate { };
+
+        public ProjectsModel(IDataService dataService)
+        {
+            Projects = new ObservableCollection<Project>();
+            foreach (Project project in dataService.GetProjects())
+            {
+                Projects.Add(project);
+            }
+        }
+
+        public void UpdateProject(IProject updatedProject)
+        {
+            GetProject(updatedProject.ID).Update(updatedProject);
+            ProjectUpdated(this,
+                new ProjectEventArgs(updatedProject));
+        }
+
+        private Project GetProject(int projectId)
+        {
+            return Projects.FirstOrDefault(
+                project => project.ID == projectId);
+        }
+    }
+}
